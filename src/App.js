7313dd881e9b262e8adb864b8ca714400a3e2867
Mohammad Fahad <mail.maohammadfahad@gmail.com>
2020-12-11 05:59:06 +0600

    Chat box added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Blog from './Components/Blog/Blog';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,6 +11,7 @@ import {
 
 function App() {
   return (
+    <>
     <Router>
         <Switch>
           <Route path='/'>
@@ -17,6 +19,9 @@ function App() {
           </Route>
         </Switch>
     </Router>
+      {/* added my burj-al-arab's appID */}
+      <MessengerCustomerChat pageId="107001421277135" appId="655923621786742" />
+    </>
   );
 }
 
